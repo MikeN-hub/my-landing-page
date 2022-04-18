75ae4e086aaf605c3625eea64583f9d3e274e90b
MikeN <mikes.navr@gmail.com>
2022-04-18 11:06:21 +0200

    homepage started

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import HomePage from './pages/HomePage/HomePage'
 import AboutPage from './pages/AboutPage/AboutPage'
 import ShopPage from './pages/ShopPage/ShopPage'
+import TodoList from './pages/TodoList/TodoList'
 import NotfoundPage from './pages/NotfoundPage/NotfoundPage'
 
 import './App.css'
@@ -13,9 +14,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />}></Route>
-          <Route path='/about' element={<AboutPage />}></Route>
-          <Route path='/shop' element={<ShopPage />}></Route>
+          <Route path='about' element={<AboutPage />}></Route>
+          <Route path='shop' element={<ShopPage />}></Route>
           <Route path='*' element={<NotfoundPage />}></Route>
+          <Route path='todolist' element={<TodoList />}></Route>
         </Route>
       </Routes>
     </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import todo_img from '../../assets/images/todo_img.jpeg'
 import styles from './HomePage.module.scss'
 
 const HomePage = () => {
   return (
     <div className={styles.homepage}>
-      <h1>You are welcome!</h1>
+      <div className='title'>
+        <h1>You are welcome!</h1>
+        <h2>Here you can explore some intresting projects</h2>
+      </div>
+      <main>
+        <div className='progects_pic'>
+          <div className='todo_pic'>
+            <img src={todo_img} alt='todo-pic' width={200} />
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
